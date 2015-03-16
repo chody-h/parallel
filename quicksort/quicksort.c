559460406176f51936338aa8f633142df37fab8c
Cody Heffner <cody.heffner@gmail.com>
2015-03-16 13:55:40 -0600

    Set up bash file.

--- quicksort/quicksort.c
+++ quicksort/quicksort.c
@@ -1,8 +1,9 @@
 #include <mpi.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <sys/time.h>
-#include <math.h>
+#include <sys/time.h>	// random 
+#include <math.h>		// log
+#include <string.h> 	// memcpy
 
 typedef enum {false, true} bool;
 
@@ -12,9 +13,11 @@ int ReverseCompare(const void*, const void*);
 
 int main(int argc, char *argv[])
 {
-	int* list, num_elements;
-	int size, max_capacity, dim, i, j, nrecv;
-	int root, partner, pivot;
+	bool debug;
+	int *list, *recv_buf, *temp;
+	double size;
+	int dim, i, j, nrecv;
+	int root, partner, pivot, count;
 	int ipiv, isend, nsend;
 
 	double starttime;
@@ -24,20 +27,19 @@ int main(int argc, char *argv[])
 	MPI_Status status;
 	MPI_Comm mycomm;
 
-	num_elements = atoi(argv[1]);
+	size = atof(argv[1]);
+	debug = (argc == 3);
 
 	MPI_Init(&argc, &argv);
 
 	MPI_Comm_size(MPI_COMM_WORLD, &nproc);
 	MPI_Comm_rank(MPI_COMM_WORLD, &iproc);
 	dim = (int)(log((double)nproc)/log(2.0));
-	size = num_elements;
-	max_capacity = size * nproc;
 
 	// initialize list with random numbers [1, 99]
-	list = malloc(max_capacity * sizeof(int));
+	if (!(list = malloc(sizeof(int) * size))) return -1;
 	srand(time(NULL)+iproc*nproc);
-	for (i = 0; i < num_elements; i++) 
+	for (i = 0; i < size; i++) 
 	{
 		list[i] = (rand() % 99) + 1;
 		// fprintf(stderr, "(%d) %d\n", iproc, list[i]);
@@ -47,32 +49,30 @@ int main(int argc, char *argv[])
 	qsort(list, size, sizeof(int), Compare);
 
 	// print lists
-	if (iproc == 0) 
+	if (debug && iproc == 0) 
 		fprintf(stderr, "\nStarting list:\n");
-	for (i = 0; i < nproc; i++) 
+	if (debug)
 	{
-		if (iproc == i)
+		for (i = 0; i < nproc; i++) 
 		{
-			fprintf(stderr, "(%d):  ", iproc);
-			for (j = 0; j < size; j++) 
+			if (iproc == i)
 			{
-				fprintf(stderr, "%d ", list[j]);
+				fprintf(stderr, "(%d):  ", iproc);
+				for (j = 0; j < size; j++) 
+				{
+					fprintf(stderr, "%d ", list[j]);
+				}
+				fprintf(stderr, "\n");
 			}
-			fprintf(stderr, "\n");
+			MPI_Barrier(MPI_COMM_WORLD);
 		}
-		MPI_Barrier(MPI_COMM_WORLD);
 	}
-
+// int debugnum = 0;
 	starttime = When();
 
-	// while not done
-		// hypercube calculations
-			// root broadcasts pivot
-			// figure out who to communicate with
-		// send half of list
-		// receive half of list
-		// merge lists
-	MPI_Comm_split(MPI_COMM_WORLD, 0, iproc, &mycomm);
+/**********************GO!************************/
+
+	mycomm = MPI_COMM_WORLD;
 	root = 0;
 	v_iproc = iproc;
 	for (i = dim-1; i >= 0; i--) 
@@ -88,61 +88,87 @@ int main(int argc, char *argv[])
 		// calculate who my partner is
 		partner = v_iproc ^ (1 << i);
 
-		// determine what to send - ties go to lower iproc
+		// left hand side
 		if (v_iproc < partner)
 		{
 			// this is the index of the first one to send
 			isend = 0;
 			while (isend < size && list[isend] <= pivot) isend++;
+
+			// send & receive the count, malloc receive buffer
+			count = size-isend;
+			MPI_Isend(&count, 1, MPI_INTEGER, partner, 0, mycomm, &request);
+			MPI_Recv(&count, 1, MPI_INTEGER, partner, 0, mycomm, &status);
+			if (!(recv_buf = malloc(sizeof(int) * count))) return -1;
+
+			// send & receive the data
 			MPI_Isend(&(list[isend]), size-isend, MPI_INTEGER, partner, 0, mycomm, &request);
 			size -= (size-isend);
-			MPI_Recv(&(list[size]), max_capacity-size, MPI_INTEGER, partner, 0, mycomm, &status);
-			MPI_Get_count(&status, MPI_INTEGER, &nrecv);
-			size += nrecv;
+			MPI_Recv(recv_buf, count, MPI_INTEGER, partner, 0, mycomm, &status);
 		}
+		// right hand side
 		else
 		{
 			// this is the count of how many to send
 			isend = size-1;
 			while (isend >= 0 && list[isend] > pivot) isend--;
 			isend++;
+
+			// send & receive the count, malloc receive buffer
+			count = isend;
+			MPI_Isend(&count, 1, MPI_INTEGER, partner, 0, mycomm, &request);
+			MPI_Recv(&count, 1, MPI_INTEGER, partner, 0, mycomm, &status);
+			if (!(recv_buf = malloc(sizeof(int) * count))) return -1;
+
+			// send and receive the data
 			MPI_Isend(list, isend, MPI_INTEGER, partner, 0, mycomm, &request);
 			qsort(list, size, sizeof(int), ReverseCompare);
 			size -= isend;
-			MPI_Recv(&(list[size]), max_capacity-size, MPI_INTEGER, partner, 0, mycomm, &status);
-			MPI_Get_count(&status, MPI_INTEGER, &nrecv);
-			size += nrecv;
+			MPI_Recv(recv_buf, count, MPI_INTEGER, partner, 0, mycomm, &status);
 		}
 
-		// sort
+		// combine the original (truncated) and received arrays
+		if (!(temp = malloc(sizeof(int) * (size + count)))) return -1;
+		memcpy(temp, list, sizeof(int)*size);
+		free(list);
+		memcpy(temp+(int)size, recv_buf, sizeof(int)*count);
+		free(recv_buf);
+
+		list = temp;
+		size += count;
+
 		qsort(list, size, sizeof(int), Compare);
+// fprintf(stderr, "(%d)debugger: %d\n", iproc, debugnum);
 
+		// divide the hypercube
 		color = (v_iproc >= pow(2, i));
-		MPI_Comm_split(mycomm, color, iproc, &mycomm);
+		MPI_Comm_split(mycomm, color, 0, &mycomm);
 		MPI_Comm_rank(mycomm, &v_iproc);
-
-		// fprintf(stderr, "(%d) pivot: %d\tindex: %d\n", iproc, pivot, isend);
 	}
+
+/**********************STOP!************************/
 
 	if (iproc == 0) 
 		fprintf(stderr, "Completed in (%f) seconds.\n", When()-starttime);
-	MPI_Barrier(MPI_COMM_WORLD);
 
 	// print lists
-	if (iproc == 0) 
+	if (debug && iproc == 0) 
 		fprintf(stderr, "\nSorted list:\n");
-	for (i = 0; i < nproc; i++) 
+	if (debug)
 	{
-		if (iproc == i)
+		for (i = 0; i < nproc; i++) 
 		{
-			for (j = 0; j < size; j++) 
+			if (iproc == i)
 			{
-				fprintf(stderr, "%d ", list[j]);
+				for (j = 0; j < size; j++) 
+				{
+					fprintf(stderr, "%d ", list[j]);
+				}
 			}
+			MPI_Barrier(MPI_COMM_WORLD);
 		}
-		MPI_Barrier(MPI_COMM_WORLD);
 	}
-	if (iproc == 0)
+	if (debug && iproc == 0)
 		fprintf(stderr, "\n");
 
 	free(list);
--- quicksort/quicksort.sh
+++ quicksort/quicksort.sh
@@ -1,10 +1,31 @@
-#mpicc quicksort.c -o quicksort -lm
+#!/bin/bash
 
-echo "2 proc, 5 numbers"
-mpirun -np 2 ./quicksort 5
-echo "4 proc, 5 numbers"
-mpirun -np 4 ./quicksort 5
-echo "8 proc, 5 numbers"
-mpirun -np 8 ./quicksort 5
-echo "16 proc, 5 numbers"
-mpirun -np 16 ./quicksort 5
+#SBATCH --time=0:10:00   # walltime
+#SBATCH --ntasks=16   # number of processor cores (i.e. tasks)
+#SBATCH --nodes=1   # number of nodes
+#SBATCH --mem-per-cpu=1024M   # memory per CPU core
+#SBATCH -J "quicksort"   # job name
+#SBATCH --mail-user=cody.heffner@gmail.com   # email address
+
+# Compatibility variables for PBS. Delete if not needed.
+# export PBS_NODEFILE=`/fslapps/fslutils/generate_pbs_nodefile`
+# export PBS_JOBID=$SLURM_JOB_ID
+# export PBS_O_WORKDIR="$SLURM_SUBMIT_DIR"
+# export PBS_QUEUE=batch
+
+# Set the max number of threads to use for programs using OpenMP. Should be <= ppn. Does nothing if the program doesn't use OpenMP.
+# export OMP_NUM_THREADS=$SLURM_CPUS_ON_NODE
+
+# LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
+#mpicc quicksort.c -o quicksort -lm
+for p in `seq 1 4`; do
+	for n in `seq 1 5`; do
+		COUNT=$((2**$n))
+		PROC=$((2**$p))
+		for iter in `seq 1 5`; do
+			echo numprocs: $PROC, numelements: $COUNT
+			mpirun -np $PROC ./quicksort $COUNT
+		done
+	done
+	echo 
+done
